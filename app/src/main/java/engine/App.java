98beff84db5fae44871fd2fcdb0c2d43dc7677e4
Nolan Prochnau <parvus.mortalis@gmail.com>
2021-05-14 13:25:05 -0400

    Fix collisions

--- app/src/main/java/engine/App.java
+++ app/src/main/java/engine/App.java
@@ -26,7 +26,7 @@ public class App extends PApplet {
   }
 
   public void settings() {
-    size(1000, 500);
+    size(1900, 500);
     this.gnd = new Ground(this, 100, 0.5f, 0.4f);
     this.square = new Square(this, 10, 0, 50);
     this.square2 = new Square(this, width / 2, 0, 100);
@@ -72,7 +72,7 @@ public class App extends PApplet {
       float p = this.square.momentum() + this.square2.momentum();
       float u = this.square.kinetic() + this.square2.kinetic();
 
-      float A = (sq(this.square2.m) / (2 * this.square.m)) - (this.square2.m / 2);
+      float A = (sq(this.square2.m) / (2 * this.square.m)) + (this.square2.m / 2);
       float B = -1 * this.square2.m * p / this.square.m;
       float C = (sq(p) / (2 * this.square.m)) - u;
 
@@ -81,6 +81,8 @@ public class App extends PApplet {
 
       this.square.setV(new PVector(v1, 0));
       this.square2.setV(new PVector(v2, 0));
+
+      println(v1, v2);
     }
   }
 
